63380cfbddd23cf6644e9e590f0a2497d0429057
pointtt <50075024+gksktl111@users.noreply.github.com>
2023-11-24 14:10:56 +0900

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Admin!</h1>
+      <h4>Your Firts page</h4>
+      <Button>asd</Button>
+    </div>
+  );
+}
